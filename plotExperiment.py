76ad2a6205f1540348121e566eede364641c717f
Emilie Zunda Primdahl Thaulow <ezpthaulow@gmail.com>
2023-11-24 21:09:55 +0100

    Fixed plots naming

--- plotExperiment.py
+++ plotExperiment.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 
 #PART1 PLOTS
 img_size = [128,320,416,496]
-accuracy = [0.024211, 0.014176, 0.013530, 0.013286]
+loss = [0.024211, 0.014176, 0.013530, 0.013286]
 
 
 # Plotting
-plt.plot(img_size, accuracy, marker='o')
+plt.plot(img_size, loss, marker='o')
 plt.title('Image size vs Performance')
 plt.xlabel('Image size')
-plt.ylabel('Accuracy')
+plt.ylabel('Validation Loss')
 plt.grid(True)
 
 # Save the plot as a PNG file
@@ -25,14 +25,14 @@ plt.clf()
 
 #PART 2 PLOTS
 num_samples = [85, 170, 255, 340, 425]
-accuracy = [0.166641, 0.098490, 0.056021, 0.040865, 0.031671]
+loss = [0.024129, 0.021603, 0.017634, 0.016905, 0.013905]
 
 
 # Plotting
-plt.plot(num_samples, accuracy, marker='o')
+plt.plot(num_samples, loss, marker='o')
 plt.title('Number of Samples vs Performance')
 plt.xlabel('Number of Samples')
-plt.ylabel('Accuracy')
+plt.ylabel('Validation Loss')
 plt.grid(True)
 
 # Save the plot as a PNG file
